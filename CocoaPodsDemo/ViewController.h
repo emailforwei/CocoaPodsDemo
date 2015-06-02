efaa9f0d988ec01da1d105b2d1e06a4abe29a180
john.wei <john@johnweideiMac.local>
2015-06-02 10:14:31 +0800

    add code

---
+++ CocoaPodsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocoaPodsDemo
+//
+//  Created by john.wei on 15/6/1.
+//  Copyright (c) 2015年 john.wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CocoaPodsDemo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  CocoaPodsDemo
+//
+//  Created by john.wei on 15/6/1.
+//  Copyright (c) 2015年 john.wei. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "AFNetworking.h"
+#import "FMDB.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
